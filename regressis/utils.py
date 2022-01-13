da8f5ea87daa89ff63afea21dfa32875d59643f2
echaussidon <edmondchau@sfr.fr>
2022-01-13 12:28:37 +0100

    convert map in float

--- regressis/utils.py
+++ regressis/utils.py
@@ -172,7 +172,7 @@ def build_healpix_map(nside, ra, dec, precomputed_pix=None, sel=None, weights=No
     if sel is None:
         sel = np.ones(precomputed_pix.size if (precomputed_pix is not None) else ra.size, dtype='?')
     pix = precomputed_pix[sel] if (precomputed_pix is not None) else hp.ang2pix(nside, ra[sel], dec[sel], nest=True, lonlat=True)
-    map = np.bincount(pix, weights=weights, minlength=hp.nside2npix(nside))
+    map = np.bincount(pix, weights=weights, minlength=hp.nside2npix(nside)) / 1.0
     if in_deg2:
         map = map / hp.nside2pixarea(nside, degrees=True)
     return map
