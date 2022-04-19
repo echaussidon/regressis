3cb9b2cb807c4a4e33ab282e1c360df3c1efc2f8
Edmond <47702632+echaussidon@users.noreply.github.com>
2022-04-19 17:34:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
+import os
+import sys
+
 from setuptools import setup
 
 
-setup(name='regressis',
-      version='0.0.1',
+package_basename = 'regressis'
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), package_basename))
+import _version
+version = _version.__version__
+
+
+setup(name=package_basename,
+      version=version,
       author='Edmond Chaussidon, Arnaud de Mattia',
       author_email='',
       description='Regression of target density against observational condition templates',
       license='GPLv3',
       url='http://github.com/echaussidon/regressis',
       install_requires=['scikit-learn', 'fitsio', 'healpy', 'astropy'],
-      package_data={'regressis': ['*.mplstyle', 'data/*']},
-      packages=['regressis']
+      package_data={package_basename: ['*.mplstyle', 'data/*']},
+      packages=[package_basename]
 )
