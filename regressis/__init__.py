1186732a33021084c4f3ff16969950657cca772a
echaussidon <edmondchau@sfr.fr>
2022-06-08 12:07:26 +0200

    add DESI footprint at nside=256

--- regressis/__init__.py
+++ regressis/__init__.py
@@ -1,5 +1,5 @@
 from .dataframe import PhotometricDataFrame
 from .regression import Regression
-from .footprint import DR9Footprint
+from .footprint import DR9Footprint, DESIFootprint
 from .utils import setup_logging
 from .weight import PhotoWeight
